ec6da7697be9c0ae8eea0a703576f350e732f918
djr4488 <djr4488@gmail.com>
2020-07-31 18:45:22 -0500

    removed some stuff that I don't think is actually needed

--- src/main/java/io/github/djr4488/parser/cdi/ParserProducer.java
+++ src/main/java/io/github/djr4488/parser/cdi/ParserProducer.java
@@ -8,45 +8,27 @@ import javax.enterprise.inject.spi.InjectionPoint;
 
 @ApplicationScoped
 public class ParserProducer {
-    private UserAgentAnalyzer fullUserAgentAnalyzer;
-    private UserAgentAnalyzer partialUserAgentAnalyzer;
-    private static final Object lock = new Object();
-
     @Produces
     @FullUserAgentAnalyzer
     public UserAgentAnalyzer getFullUserAgentAnalyzer() {
-        if (fullUserAgentAnalyzer == null) {
-            synchronized (lock) {
-                if (fullUserAgentAnalyzer == null) {
-                    fullUserAgentAnalyzer = UserAgentAnalyzer.newBuilder()
-                            .hideMatcherLoadStats()
-                            .withoutCache()
-                            .build();
-                }
-            }
-        }
-        return fullUserAgentAnalyzer;
+        return UserAgentAnalyzer.newBuilder()
+                .hideMatcherLoadStats()
+                .withoutCache()
+                .build();
     }
 
     @Produces
     @PartialUserAgentAnalyzer
     public UserAgentAnalyzer getPartialUserAgentAnalyzer(InjectionPoint injectionPoint) {
-        if (partialUserAgentAnalyzer == null) {
-            synchronized (lock) {
-                if (partialUserAgentAnalyzer == null) {
-                    PartialUserAgentAnalyzer annotation = injectionPoint.getAnnotated().getAnnotation(PartialUserAgentAnalyzer.class);
-                    String[] fields = annotation.value();
-                    UserAgentAnalyzer.UserAgentAnalyzerBuilder uaab =
-                            UserAgentAnalyzer.newBuilder()
-                                             .hideMatcherLoadStats()
-                                             .withoutCache();
-                    for (String field : fields) {
-                        uaab.withField(field);
-                    }
-                    partialUserAgentAnalyzer = uaab.build();
-                }
-            }
+        PartialUserAgentAnalyzer annotation = injectionPoint.getAnnotated().getAnnotation(PartialUserAgentAnalyzer.class);
+        String[] fields = annotation.value();
+        UserAgentAnalyzer.UserAgentAnalyzerBuilder uaab =
+                UserAgentAnalyzer.newBuilder()
+                                 .hideMatcherLoadStats()
+                                 .withoutCache();
+        for (String field : fields) {
+            uaab.withField(field);
         }
-        return partialUserAgentAnalyzer;
+        return uaab.build();
     }
 }
